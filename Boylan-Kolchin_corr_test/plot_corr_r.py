c5490f4acd085dc1c4199c64ec1ea67629f00f9e
Jean P Walker <jpwalker4039@gmail.com>
2014-01-24 21:17:02 -0500

    01-24-14

--- Boylan-Kolchin_corr_test/plot_corr_r.py
+++ Boylan-Kolchin_corr_test/plot_corr_r.py
@@ -1,4 +1,5 @@
-##Plot the 2-pt autocorrelation function from Boylan-Kolchin
+##Plot the 2-pt autocorrelation function from Boylan-Kolchin and xi_m_m found from Komatsu's fortran code.
+##This was just meant as a test of the Komatsu's code. This shows we didn't get a perfect fit to the functions given to use from Boylan. 
 import os
 import IO
 import matplotlib.pyplot as plt
@@ -7,13 +8,14 @@ if __name__ == '__main__':
 	cwd = '{0}/'.format(os.getcwd())
 	correlation_location = 'correlations/'
 	files = ['xi_m_m.txt', 'correl_067_millen.txt', 'correl_067_mini.txt', 'correl_067.txt']
+	#readfile(filename, col = 2, delim = ' ', skip = -1, numlines = -1)
 	for i in files:
 		if i != 'xi_m_m.txt':
 		#0 element has r (Mpc/h); 1 element is xi This is only true for Boylan-Kolchin files
-			data_i = IO.readfile('{0}{1}{2}'.format(cwd, correlation_location, i), 4, ' ', 2)
+			data_i = IO.readfile('{0}{1}{2}'.format(cwd, correlation_location, i), 4, ' ', skip = 2)
 			symbol = ''
 		else:
-			data_i = IO.readfile('{0}{1}{2}'.format(cwd, correlation_location, i), 2, ' ')
+			data_i = IO.readfile('{0}{1}{2}'.format(cwd, correlation_location, i), 2, ',')
 			symbol = '*'
 		plt.plot(data_i[0], data_i[1], symbol, label = i)
 	plt.xlabel('r (Mpc / h)')
--- src/mstar_test.py
+++ src/mstar_test.py
@@ -4,103 +4,32 @@ Created on Jan 10, 2014
 @author: jpwalker
 '''
 
-from IO import readfile 
 import matplotlib.pyplot as plt
 import numpy as np
 import MillenniumII as m2
-from subprocess import check_output
 import os
-from collections import namedtuple
-from matplotlib import rc
-
-corr_func = namedtuple('corr_func', ['r', 'cf', 'err', 'DD', 'RR'])
-
-def create_corr_struct(r = [], cf = [], err = [], DD = [], RR = [], num = 0):
-    if len(r) == len(cf) and len(cf) == len(err) and len(err) == len(RR) and len(RR) == len(DD):
-        cast(r, 'flt')
-        cast(cf, 'flt')
-        cast(RR, 'flt')
-        cast(DD, 'flt')
-        cast(err, 'flt')
-        cast(num, 'int')
-        return {'num': 0, 'data': corr_func(r, cf, err, DD, RR)}
-    else:
-        raise TypeError('Input array length mismatch')
-
-def cast(d, t):
-    ret = np.zeros(len(d))
-    try:
-        for (j, i) in enumerate(d):
-            if t == 'int':
-                ret[j] = int(i)
-            if t == 'flt':
-                ret[j] =  float(i)
-    except ValueError:
-        print('Unable to cast {0}'.format(i))
-    return ret
-        
-def read_corr_file(filen):
-    num = readfile(filen, 1, skip = 1, numlines = 1)
-    data = readfile(filen, 4, ',', skip = 2)
-    return create_corr_struct(data[0], data[1], np.zeros(len(data[0])), data[2], data[3], num)
-    
-def calc_auto_corr(mill_data, outfile = '', tempfile_addon = ''):
-    curr = '{0}/'.format(os.getcwd())
-    if outfile == '':
-        outfile = '{0}outtemp'.format(curr)
-        wr = False
-    temp_file = '{0}temp{1}'.format(curr, tempfile_addon)
-    m2.write_halo_table_ascii(temp_file, mill_data, fmt = '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20')
-    check_output(['2pt-autocorrelation-MS', temp_file, outfile])
-    auto_corr = read_corr_file(outfile)
-    os.remove(temp_file)
-    if not wr:
-        os.remove(outfile)
-    return auto_corr
-
-#===============================================================================
-# def calc_cross_corr(mill_data, outfile = '' tempfile_addon = ''):
-#     curr = '{0}/'.format(curr)
-#     if outfile == '':
-#         outfile = '{0}outtemp'.format(curr)
-#         wr = False
-#     temp_file = '{0}temp{1}'.format(curr, tempfile_addon)
-#     m2.write_halo_table_ascii(temp_file, mill_data, '7,8,9')
-#===============================================================================
-    
-    
-def check_for_pos_key(key):
-    if not (key == 'x' or key == 'y' or key == 'z'):
-        raise TypeError('Input key: {0} is not valid').format(key)
-
-##function plots Millennium or MillenniumII structures 
-def plot_position_scatter(MS_table, pos_key1 = 'x', pos_key2 = 'y'):
-    check_for_pos_key(pos_key1)
-    check_for_pos_key(pos_key2)
-    pos1 = m2.get_col_halo_table(MS_table, pos_key1)
-    pos2 = m2.get_col_halo_table(MS_table, pos_key2)
-    plt.plot(pos1, pos2, '*', markersize = .96)
-    plt.xlabel('{0} [Mpc / h]'.format(pos_key1))
-    plt.ylabel('{0} [Mpc / h]'.format(pos_key2))
-    plt.show()
+import Correlation_Func as cf
+import Standard_Plots as sp
     
 if __name__ == '__main__':
     curr = '{0}/'.format(os.getcwd())
-    filen = 'Mstar_cat2.txt'
+    filen = 'Mstar_cat_snap31-MS2.txt'
     xi_m_m_filen = 'xi_m_m_boylan.txt'
     h = 0.73
-    mass_conv = 8.6E8 / h #Converts to Solar Masses 
+    mass_conv = 6.9E6 / h #Converts to Solar Masses 
     #Read in xi_m_m
-    xi_m_m_corr = read_corr_file(xi_m_m_filen)
+    xi_m_m_corr = cf.read_corr_file(xi_m_m_filen)
     #Read in sample
     sample = m2.read_halo_table_ascii('{0}{1}'.format(curr, filen), '7,8,9,17')
     print 'Plotting position scatter plot of sample file: {0}'.format(filen)
-    plot_position_scatter(sample, 'x', 'y')
+    sp.plot_position_scatter(sample, 'x', 'y')
+    plt.show()
     print np.median(np.array(m2.get_col_halo_table(sample, 'fof_np')) * mass_conv)
-    sample_auto_corr = calc_auto_corr(sample)
+    sample_auto_corr = cf.calc_auto_corr(sample, MS2 = True)
     #sample_cross_corr = calc_cross_corr(sample)
     plt.loglog(sample_auto_corr['data'].r, sample_auto_corr['data'].cf, label = 'sample')
     plt.loglog(xi_m_m_corr['data'].r, xi_m_m_corr['data'].cf, label = '$\\xi_{mm}$')
+    #plt.
     plt.plot(sample_auto_corr['data'].r, np.sqrt(sample_auto_corr['data'].cf / xi_m_m_corr['data'].cf))
     plt.legend()
     plt.rc('text', usetex = True)
