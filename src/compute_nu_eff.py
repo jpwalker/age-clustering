bec9478dc33a9086ae3ba1dcd23854c67271f88f
jpwalker <jpwalker@192.168.0.113>
2014-03-04 10:38:51 -0500

    3-4-14

--- src/compute_nu_eff.py
+++ src/compute_nu_eff.py
@@ -4,5 +4,16 @@ Created on Feb 18, 2014
 @author: jpwalker
 '''
 
+import numpy as np
+
+def calc_nu_eff(nu, bias, nu_med, bias_med):
+    
+
+def calc_seljak_warren(num):
+    Mlow = 1E10
+    Mhigh = 1E15
+    M = np.logspace(Mlow, Mhigh, num)
+    
+
 if __name__ == '__main__':
     
