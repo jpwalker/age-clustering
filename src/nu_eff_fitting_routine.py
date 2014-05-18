aecb3e441df80663b5e4698aa47163d60263337e
jpwalker <jpwalker@192.168.0.113>
2014-05-18 13:13:26 -0400

    5-18-14

--- src/nu_eff_fitting_routine.py
+++ src/nu_eff_fitting_routine.py
@@ -26,19 +26,24 @@ if __name__ == '__main__':
     zs =  [6.196857, 4.179475, 2.0700316, 0.98870987, 0] #INPUT
     home = '{0}/'.format(os.environ['HOME'])
     snaps = [22, 27, 36, 45, 67]
+    identifier = '-1'
     (nu_no_age, bias_no_age)  = cmpn.calc_seljak_warren(1000, cosmo)
-    xtot = []
-    ytot = []
-    for (t, s) in enumerate(snaps):
-        z = zs[t]
-        finaldir = '{0}Desktop/age-clustering-data/snap{1}/attempt1_sub_form_gao/'.format(home, s) ##INPUT
-        agelabel = 'Sub-Root-Form. Age' ##INPUT
-        col_j = ['k', 'b', 'c', 'g', 'm', 'r'] ##Predefined colors for age_i
-        nu_res = []
-        for age_i in range(0,6):
+    col_j = ['k', 'b', 'c', 'g', 'm', 'r'] ##Predefined colors for age_i
+    nu_res = []
+    ages = np.array([], dtype = float)
+    fractional_age = []
+    param1 = []
+    param2 = []
+    for age_i in range(0,6):
+        xtot = []
+        ytot = []
+        for (t, s) in enumerate(snaps):
+            z = zs[t]
+            finaldir = '{0}Desktop/age-clustering-data/snap{1}{2}/attempt1_sub_form_gao/'.format(home, s, identifier) ##INPUT
+            agelabel = 'Sub-Root-Form. Age' ##INPUT
             x = np.array([])
             y = np.array([])
-            txt = np.array([],dtype = int)
+            txt = np.array([], dtype = int)
             if age_i == 0:
                 temp = cmpn.nu_eff(finaldir, age_i, range(1, 8), cosmo, z, nu_no_age, bias_no_age)
                 median_age = temp[5]
@@ -48,7 +53,8 @@ if __name__ == '__main__':
                 for (idx, x_temp) in enumerate(nu_res[-1][5]):
                     if nu_res[-1][4][idx] > -100:
                         idx2 = np.where(mass_i_median_age == nu_res[-1][0][idx])[0]
-                        x = np.append(x, nu_res[-1][2][idx]) 
+                        ages = np.append(ages, (nu_res[-1][5][idx] - median_age[idx2]) / median_age[idx2])
+                        x = np.append(x, nu_res[-1][2][idx])
                         y = np.append(y, nu_res[-1][4][idx])
                         txt = np.append(txt, nu_res[-1][0][idx])
                 xtot.extend(x)
@@ -57,17 +63,32 @@ if __name__ == '__main__':
                 #         color = col_j[age_i], label = '{0}_{1}_{2}'.format(agelabel, s, age_i))
                 #for (i, txt_i) in enumerate(txt):
                 #    plt.text(x[i], y[i], txt_i)
-    plt.plot([0,10],[0,10], '--')
-    best_fit_param = leastsq(fitting_func, (1.0, 0.8), args = (xtot, ytot))
-    x = np.array(xtot) - best_fit_param[0][1]
-    y = np.array(ytot) - best_fit_param[0][1]
-    plt.plot(x / (1. + np.exp(-best_fit_param[0][0] * x)), y, 'k+')
-    plt.xlabel('(nu - {1}) / (1 + e^(-{0} * (nu - {1})))'.format(best_fit_param[0][0], best_fit_param[0][1]))
-    plt.ylabel('nu_eff - {0}'.format(best_fit_param[0][1]))
-    #plt.legend()
+        #done running through all snapshots for each age_i
+        if age_i != 0:
+            plt.plot([0,10],[0,10], 'k--')
+            fractional_age.append(np.median(ages))
+            best_fit_param = leastsq(fitting_func, (1.0, 0.8), args = (xtot, ytot))
+            param1.append(best_fit_param[0][0])
+            param2.append(best_fit_param[0][1])
+            x = np.array(xtot) - best_fit_param[0][1]
+            y = np.array(ytot) - best_fit_param[0][1]
+            plt.plot(x + best_fit_param[0][1], y + best_fit_param[0][1], '{0}*'.format(col_j[age_i]), label = str(age_i))
+            plt.plot(x + best_fit_param[0][1], x / (1.+ np.exp(-best_fit_param[0][0] * x)) + best_fit_param[0][1], '{0}+'.format(col_j[age_i]))
+            #plt.plot(x / (1. + np.exp(-best_fit_param[0][0] * x)), y, '{0}*'.format(col_j[age_i]), label = str(age_i))
+            plt.xlabel('(nu - {1}) / (1 + e^(-{0} * (nu - {1})))'.format(best_fit_param[0][0], best_fit_param[0][1]))
+            plt.ylabel('nu_eff - {0}'.format(best_fit_param[0][1]))
+    plt.legend()
     plt.show()
-    plt.plot(xtot, ytot, 'k+')
-    plt.plot(xtot, x / (1.+ np.exp(-best_fit_param[0][0] * x)) + best_fit_param[0][1], 'r+')
-    plt.xlabel('nu')
-    plt.ylabel('nu_eff')
+    plt.plot(fractional_age, param1, '*')
+    plt.xlabel('(age - <age>) / <age>')
+    plt.ylabel('param_1')
     plt.show()
+    plt.plot(fractional_age, param2, '*')
+    plt.xlabel('(age - <age>) / <age>')
+    plt.ylabel('param_2')
+    plt.show()
+#     plt.plot(xtot, ytot, 'k+')
+#     plt.plot(xtot, x / (1.+ np.exp(-best_fit_param[0][0] * x)) + best_fit_param[0][1], 'r+')
+#     plt.xlabel('nu')
+#     plt.ylabel('nu_eff')
+#     plt.show()
