d9ce8accc971c6b52ee1b5d468db6e255266a140
jpwalker <jpwalker@192.168.0.113>
2014-05-21 11:16:24 -0400

    5-21-14

--- plots/plot_bias_r.py
+++ plots/plot_bias_r.py
@@ -3,46 +3,54 @@
 import matplotlib.pyplot as plt
 from MillenniumII import *
 from IO import *
+import Correlation_Func as corr
 import os
 
+snap = 36
+snap_post = '-1'
 home = '{0}/'.format(os.environ['HOME'])
-direc = '{0}Desktop/age-clustering-data/age-clustering attempt 2/z0_attempt1_form_jp/'.format(home)
-infile = 'millenniumIIsnap67age_attempt1057fof2.txt'
+agedirec = 'attempt1_sub_form_gao'
+direc = '{0}Desktop/age-clustering-data/snap{1}{2}/{3}/'.format(home, snap, snap_post, agedirec)
+infile = 'attempt1millenniumII_sub.txt'
 outfile = 'bias_properties.dat'
+xscale = (5, 25)
+yscale = (0.5, 2.7)
 h = 0.73
 massconv = 6.885e6 #Mass conversion reports mass in M_sun
 #Read in the matter matter 2pt-autocorrelation function
-xi_m_m = readfile('{0}xi_m_m.txt'.format(direc), col = 2, delim = ' ')
+xi_m_m = corr.read_corr_file('{0}xi_m_m.txt'.format(direc))
 #Read in the properties file were mass and age properties are stored.
 prop = readfile('{0}properties.dat'.format(direc), col = 28, delim = '    ', skip = 1)
 plt.figure(1)
 temp1 = 3
 temp2 = 3
-j = [1, 2, 3, 4, 5] #Age bins which will be plotted
-col_j = ['b', 'c', 'g', 'm', 'r'] #Colors of Age bins that are plotted
+j = [0, 1, 2, 3, 4, 5] #Age bins which will be plotted
+col_j = ['k', 'b', 'c', 'g', 'm', 'r'] #Colors of Age bins that are plotted
 #lbl_coords = [[0.25, 0.65, 0.25, 0.65], [0.85, 0.85, 0.4, 0.4]] #Coords of labels on plot
 stored_bias = [[], [], []]
-for mass_i in range(1, 8):
+for mass_i in range(1, 9):
     plt.subplot(temp1, temp2, mass_i)
-    plt.axhline(1., linestyle = '-', linewidth = 2)
-    mass = []
+    plt.axhline(1., linestyle = 'k-', linewidth = 2)
     for (i, age_i) in enumerate(j):
         idx = np.where(np.logical_and(prop[0] == mass_i, prop[1] == age_i))[0][0]
-        mass.extend([prop[3][idx], prop[6][idx]])
+        mass = np.array([prop[3][idx], prop[6][idx]]) * massconv
         bias = readfile('{0}bias/bias_{1}_{2}'.format(direc, mass_i, age_i), col = 2, delim = ',', skip = 1)
         idx = np.where(np.logical_and(np.logical_and(bias[0] >= 5., bias[0] <= 25.), bias[1] == bias[1]))
         stored_bias[2].append(np.sum(bias[1][idx]) / len(bias[1][idx]))
         stored_bias[0].append(mass_i)
         stored_bias[1].append(age_i)
-        #plt.figtext(lbl_coords[0][mass_i - 1], lbl_coords[1][mass_i - 1] - .03 * (i + 1), \
- #               '[bias_{0}_{1}: {2}]'.format(mass_i, age_i, bias_avg), color = col_j[i])
+        plt.title('{0:.2E} <= M < {1:.2E} [M_Sun / h]'.format(mass[0], mass[1]), fontsize = 'medium')
+#         plt.figtext(lbl_coords[0][mass_i - 1], lbl_coords[1][mass_i - 1] - .03 * (i + 1), \
+#                '[bias_{0}_{1}: {2}]'.format(mass_i, age_i, stored_bias[2][-1]), color = col_j[i])
         plt.plot(bias[0], bias[1], linewidth = 3.5, color = col_j[i])
+        plt.xlim(xscale)
+        plt.ylim(yscale)
         plt.xlabel('r [Mpc / h]')
         plt.ylabel('b(r)')
-    mass = np.array(mass) * massconv
-    n = int(np.log10(max(mass)))
-    #plt.figtext(lbl_coords[0][mass_i - 1], lbl_coords[1][mass_i - 1], \
-     #           '[{0:1.1f}, {1:1.1f}] x 10^{2} M_sun/h'.format(min(mass) / 10.** n, max(mass) / 10. ** n, n))
+#         mass = np.array(mass) * massconv
+#         n = int(np.log10(max(mass)))
+#         plt.figtext(lbl_coords[0][mass_i - 1], lbl_coords[1][mass_i - 1], \
+#                '[{0:1.1f}, {1:1.1f}] x 10^{2} M_sun/h'.format(min(mass) / 10.** n, max(mass) / 10. ** n, n))
 plt.show()
 stored_bias = np.array(stored_bias)
 writefile('{0}{1}'.format(direc, outfile), stored_bias, delim = ' ', note = 'mass_i    age_i    bias')
--- src/nu_eff_fitting_routine_fixed_transition.py
+++ src/nu_eff_fitting_routine_fixed_transition.py
@@ -1,6 +1,6 @@
 '''
 Created on Apr 20, 2014
-
+Fit nu_eff(nu) with a fixed transition in the break off.
 @author: jpwalker
 '''
 
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 from scipy.optimize import leastsq
 from math import exp
 
-def fitting_func1((A, B), ls, rs):
-    #ls is x and rs is y
+def fitting_func1((A), ls, rs, B):
+    #ls =nu and rs=nu_eff
     ret = []
     for i in range(len(ls)):
-        x = ls[i] - B
-        y = rs[i] - B
-        ret.append(y - x / (1. + exp(-A * x)))
+        x = ls[i] - A[0]
+        y = rs[i] - A[0]
+        ret.append(y - x / (1. + exp(-B * x)))
     return ret
 
 def fitting_func2((m1, b1, m2, b2), ls, rs, B):
@@ -32,13 +32,14 @@ def fitting_func2((m1, b1, m2, b2), ls, rs, B):
         g = 1. / (1. + exp(-B * x[i]))
         ret[i] = rs - (m2 * ls[i] + b2 + ((m1 - m2) * ls[i] + b1 - b2) * g)
     return ret
-
+    
 if __name__ == '__main__':
+    fixed_transition_model = 200.
     cosmo = {'omega_M_0': 0.25, 'omega_lambda_0': 0.75, 'omega_b_0': 0.045, \
              'h': 0.73, 'sigma_8': 0.9, 'n': 1.0, 'omega_n_0': 0., 'N_nu': 0} # INPUT
-    zs =  [0.98870987]#[6.196857, 4.179475, 2.0700316, 0.98870987, 0] #INPUT
+    zs =  [2.0700316]#[6.196857, 4.179475, 2.0700316, 0.98870987, 0] #INPUT
     home = '{0}/'.format(os.environ['HOME'])
-    snaps = [45]#[22, 27, 36, 45, 67]
+    snaps = [36]#[22, 27, 36, 45, 67]
     identifier = '-1'
     (nu_no_age, bias_no_age)  = cmpn.calc_seljak_warren(1000, cosmo)
     col_j = ['k', 'b', 'c', 'g', 'm', 'r'] ##Predefined colors for age_i
@@ -84,26 +85,22 @@ if __name__ == '__main__':
         if age_i != 0:
             plt.plot([0,10],[0,10], 'k--')
             fractional_age.append(np.median(ages))
-            best_fit_param = leastsq(fitting_func1, (1.0, 0.8), args = (xtot, ytot))
+            best_fit_param = leastsq(fitting_func1, (0.8), args = (xtot, ytot, fixed_transition_model))
             param1.append(best_fit_param[0][0])
-            param2.append(best_fit_param[0][1])
-            print 'Fitted age_i: {0}, snapshots: {1}, v_0 = {2}, A = {3}'.format(age_i, snaps, param2[-1], param1[-1])
-            x = np.array(xtot) - best_fit_param[0][1]
-            y = np.array(ytot) - best_fit_param[0][1]
-            plt.plot(x + best_fit_param[0][1], y + best_fit_param[0][1], '{0}*'.format(col_j[age_i]), label = str(age_i))
-            plt.plot(x + best_fit_param[0][1], x / (1.+ np.exp(-best_fit_param[0][0] * x)) + best_fit_param[0][1], '{0}+'.format(col_j[age_i]))
+            #param2.append(best_fit_param[0][1])
+            print 'Fitted age_i: {0}, snapshots: {1}, v_0 = {2}, A = {3}'.format(age_i, snaps, param1[-1], fixed_transition_model)
+            x = np.array(xtot) - param1[-1]
+            y = np.array(ytot) - param1[-1]
+            plt.plot(x + param1[-1], y + param1[-1], '{0}*'.format(col_j[age_i]), label = str(age_i))
+            plt.plot(x + param1[-1], x / (1.+ np.exp(-fixed_transition_model * x)) + param1[-1], '{0}+'.format(col_j[age_i]))
             #plt.plot(x / (1. + np.exp(-best_fit_param[0][0] * x)), y, '{0}*'.format(col_j[age_i]), label = str(age_i))
-            plt.xlabel('(nu - {1}) / (1 + e^(-{0} * (nu - {1})))'.format(best_fit_param[0][0], best_fit_param[0][1]))
-            plt.ylabel('nu_eff - {0}'.format(best_fit_param[0][1]))
+            plt.xlabel('(nu - {1}) / (1 + e^(-{0} * (nu - {1})))'.format(fixed_transition_model, param1[-1]))
+            plt.ylabel('nu_eff - {0}'.format(param1[-1]))
     plt.legend()
     plt.show()
     plt.plot(fractional_age, param1, '*')
     plt.xlabel('(age - <age>) / <age>')
     plt.ylabel('param_1')
-    plt.show()
-    plt.plot(fractional_age, param2, '*')
-    plt.xlabel('(age - <age>) / <age>')
-    plt.ylabel('param_2')
     plt.show()
 #     plt.plot(xtot, ytot, 'k+')
 #     plt.plot(xtot, x / (1.+ np.exp(-best_fit_param[0][0] * x)) + best_fit_param[0][1], 'r+')
