7822028d59a9017161fda487a312f99317085d6b
jpwalker <jpwalker@192.168.6.137>
2014-06-11 16:58:32 -0400

    6-11-14

--- src/nu_eff_fitting_routine_fixed_transition.py
+++ src/nu_eff_fitting_routine_fixed_transition.py
@@ -59,6 +59,16 @@ def param_fit_func((A, B, C), x, y):
     for i in range(len(x)):
         ret[i] = y[i] - (A * exp(B * x[i]) + C)
     return ret
+
+def index_nu_eff(data, a_i, m_i):
+    idx = set()
+    idx2 = set()
+    for age_i in a_i:
+        idx = idx.union(set(np.where(data[1] == age_i)[0]))
+    for mass_i in m_i:
+        idx2 = idx2.union(set(np.where(data[0] == mass_i)[0]))
+    ret = idx2.intersection(idx)
+    return np.array(list(ret))
     
 if __name__ == '__main__':
     fixed_transition_model = 300.
@@ -71,89 +81,79 @@ if __name__ == '__main__':
     snap_identifier = '-1'
     (nu_no_age, bias_no_age)  = cmpn.calc_seljak_warren(1000, cosmo)
     col_j = ['k', 'b', 'c', 'g', 'm', 'r'] ##Predefined colors for age_i
-    
-    ##Folowing variables are used to find the median age of a mass_i, snap sample.
-    median_age = np.empty(0, dtype = float)
-    mass_i_median_age = np.empty(0, dtype = float)
-    snap_median_age = np.empty(0, dtype = int)
-    
+    p1 = np.empty(len(snaps), dtype = np.object)
     #Properties for the mass-age sample across all redshifts
-    tot_age = np.empty(0, dtype = float)
-    tot_nueff = np.empty(0, dtype = float)
-    tot_nu = np.empty(0, dtype = float)
-    tot_z = np.empty(0, dtype = float)
-    medianage_agei = np.empty(0, dtype = float)
-    testnu0 = np.empty(0, dtype = float)
     for (t, s) in enumerate(snaps): #Step through redshift
-        #Properties for each age_i seperately
-        ages_agei = np.empty(0, dtype = float)
-        nu_agei = np.empty(0, dtype = float) 
-        nueff_agei = np.empty(0, dtype = float)
-        z_agei = np.empty(0, dtype = float)
         z = zs[t]
         pnt = z_points[t]
         finaldir = '{0}Desktop/age-clustering-data/snap{1}{2}/attempt1_sub_form_gao/'.format(home, s, snap_identifier) ##INPUT
         agelabel = 'Sub-Root-Form. Age' ##INPUT ##Label for age definition
-        if age_i == 0:
-            temp = cmpn.nu_eff(finaldir, age_i, range(1, 8), cosmo, z, nu_no_age, bias_no_age)
-            median_age = np.append(median_age, temp[5])
-            mass_i_median_age = np.append(mass_i_median_age, temp[0])
-            snap_median_age = np.append(snap_median_age, np.ones(len(temp[0])) * s)
-        else:
-            nu_res = cmpn.nu_eff(finaldir, age_i, range(1, 8), cosmo, z, nu_no_age, bias_no_age)
-            for (idx, x_temp) in enumerate(nu_res[5]): #Step through mass_i and enumerate the age
+        nu_res = cmpn.nu_eff(finaldir, range(0, 6), range(1, 8), cosmo, z, nu_no_age, bias_no_age)
+        for age_i in range(1, 6): #Step through mass_i and enumerate the age
+            tot_agei = np.empty(0, dtype= int)
+            tot_massi = np.empty(0, dtype = int)
+            tot_age = np.empty(0, dtype = float)
+            tot_nueff = np.empty(0, dtype = float)
+            tot_nu = np.empty(0, dtype = float)
+            tot_z = np.empty(0, dtype = float)
+            color = col_j[age_i]
+            for mass_i in range(1, 8):
+                idx = index_nu_eff(nu_res, [age_i], [mass_i])[0]
                 if nu_res[4][idx] > -100: ##If there is a nu_eff calculated for this object
-                        idx2 = np.where(np.logical_and(mass_i_median_age == nu_res[0][idx], snap_median_age == s))[0][0]
-                        #agei parameters
-                        ages_agei = np.append(ages_agei, (x_temp - median_age[idx2]) / median_age[idx2])
-                        nu_agei = np.append(nu_agei, nu_res[2][idx])
-                        nueff_agei = np.append(nueff_agei, nu_res[4][idx])
-                        z_agei = np.append(z_agei, t) #this is the index of the redshift for this sample
-                        #parameters for all mass-age samples
-                        tot_age = np.append(tot_age, ages_agei[-1]) #Calculate the fractional age 
-                        tot_nu = np.append(tot_nu, nu_res[2][idx])
-                        tot_nueff = np.append(tot_nueff, nu_res[4][idx])
-                        tot_z = np.append(tot_z, t) #this is the index of the redshift for this sample
+                    idx2 = index_nu_eff(nu_res, [0], [mass_i])[0]
+                    #Calculated values for mass_i and age_i sample
+                    tot_agei = np.append(tot_agei, nu_res[1][idx])
+                    tot_massi = np.append(tot_massi, nu_res[0][idx])
+                    tot_age = np.append(tot_age, (nu_res[5][idx] - nu_res[5][idx2]) / nu_res[5][idx2]) #Calculate the fractional age 
+                    tot_nu = np.append(tot_nu, nu_res[2][idx])
+                    tot_nueff = np.append(tot_nueff, nu_res[4][idx])
+                    tot_z = np.append(tot_z, t) #this is the index of the redshift for this sample
+            p1[t] = plt.plot(tot_nu, tot_nueff, '{0}{1}'.format(pnt, color))[0]
+    plt.legend(p1, zs, bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+    plt.xlabel('(age - <age>) / <age>')
+    plt.ylabel('nu_eff')
+    plt.show()
+        
+        
+        
+        
         #done running through all snapshots for each age_i
-        if age_i != 0:
-            medianage_agei = np.append(medianage_agei, np.median(ages_agei))
-            testnu0 = np.append(testnu0, leastsq(fitting_func3, (1.), args = (nu_agei, nueff_agei, fixed_transition_model))[0][0])
-            nu0 = 0.03390915 * np.exp(10.491329 * ages_agei) + 0.74143699
-            #param1.append(best_fit_param[0][:])
-            #nu0 = param1[-1][0] * np.exp(param1[-1][1] * ages) + param1[-1][2]
-            #param2.append(best_fit_param[0][1])
-            #print 'Fitted age_i: {0}, snapshots: {1}, A1 = {3}, Parameters: {4}, {5}, {6}'.format(age_i, snaps, fixed_transition_model, param1[-1][0], param1[-1][1], param1[-1][2])            
-            x = nu_agei - nu0
-            y = nueff_agei - nu0
-            pltx = x / (1. + np.exp(-fixed_transition_model * x))
-            plty = y
-            sort_idx = np.argsort(pltx)
-            plt.plot(pltx[sort_idx], plty[sort_idx], '{0}-'.format(col_j[age_i]))
-
-    temp = [min(plt.xlim()[0], plt.ylim()[0]), max(plt.xlim()[1], plt.ylim()[1])]
-    plt.plot(temp, temp, 'k--') #Plot y=x line
-    plt.xlabel('x * g(x)')
-    plt.ylabel('y')
-    plt.show()
-    
-    plt.plot(medianage_agei, testnu0, '*k')
-    nu0_params = leastsq(param_fit_func, (0.01, 10., 5.), args = (medianage_agei, testnu0))[0]
-    plt.plot(medianage_agei, nu0_params[0] * np.exp(medianage_agei * nu0_params[1]) + nu0_params[2], '-k')
-    plt.xlabel('median_age')
-    plt.ylabel('nu0')
-    plt.show()
-    
-    param = leastsq(fitting_func1, (1., 1., 1.), args = (tot_nu, tot_nueff, fixed_transition_model, tot_age))
-    nu0 = param[0][0] * np.exp(param[0][1] * tot_age) + param[0][2]
-    x = tot_nu - nu0
-    y = tot_nueff - nu0
-    pltx = x + nu0 #/ (1. + np.exp(-fixed_transition_model * x))
-    plty = y + nu0
-    sort_idx = np.argsort(pltx)
-    plt.plot(pltx, plty - (x / (1. + np.exp(-fixed_transition_model * x)) + nu0), 'k*')
-    #plt.plot(pltx, x / (1. + np.exp(-fixed_transition_model * x)) + nu0, 'r*')
-    temp = [min(plt.xlim()[0], plt.ylim()[0]), max(plt.xlim()[1], plt.ylim()[1])]
-    plt.plot(temp, temp, 'k--') #Plot y=x line
-    plt.xlabel('x * g(x)')
-    plt.ylabel('y')
-    plt.show()
+#         nu0 = 0.03390915 * np.exp(10.491329 * ages_agei) + 0.74143699
+#         #param1.append(best_fit_param[0][:])
+#             #nu0 = param1[-1][0] * np.exp(param1[-1][1] * ages) + param1[-1][2]
+#             #param2.append(best_fit_param[0][1])
+#             #print 'Fitted age_i: {0}, snapshots: {1}, A1 = {3}, Parameters: {4}, {5}, {6}'.format(age_i, snaps, fixed_transition_model, param1[-1][0], param1[-1][1], param1[-1][2])            
+#             x = nu_agei - nu0
+#             y = nueff_agei - nu0
+#             pltx = x / (1. + np.exp(-fixed_transition_model * x))
+#             plty = y
+#             sort_idx = np.argsort(pltx)
+#             plt.plot(pltx[sort_idx], plty[sort_idx], '{0}-'.format(col_j[age_i]))
+# 
+#     temp = [min(plt.xlim()[0], plt.ylim()[0]), max(plt.xlim()[1], plt.ylim()[1])]
+#     plt.plot(temp, temp, 'k--') #Plot y=x line
+#     plt.xlabel('x * g(x)')
+#     plt.ylabel('y')
+#     plt.show()
+#     
+#     plt.plot(medianage_agei, testnu0, '*k')
+#     nu0_params = leastsq(param_fit_func, (0.01, 10., 5.), args = (medianage_agei, testnu0))[0]
+#     plt.plot(medianage_agei, nu0_params[0] * np.exp(medianage_agei * nu0_params[1]) + nu0_params[2], '-k')
+#     plt.xlabel('median_age')
+#     plt.ylabel('nu0')
+#     plt.show()
+#     
+#     param = leastsq(fitting_func1, (1., 1., 1.), args = (tot_nu, tot_nueff, fixed_transition_model, tot_age))
+#     nu0 = param[0][0] * np.exp(param[0][1] * tot_age) + param[0][2]
+#     x = tot_nu - nu0
+#     y = tot_nueff - nu0
+#     pltx = x + nu0 #/ (1. + np.exp(-fixed_transition_model * x))
+#     plty = y + nu0
+#     sort_idx = np.argsort(pltx)
+#     plt.plot(pltx, plty - (x / (1. + np.exp(-fixed_transition_model * x)) + nu0), 'k*')
+#     #plt.plot(pltx, x / (1. + np.exp(-fixed_transition_model * x)) + nu0, 'r*')
+#     temp = [min(plt.xlim()[0], plt.ylim()[0]), max(plt.xlim()[1], plt.ylim()[1])]
+#     plt.plot(temp, temp, 'k--') #Plot y=x line
+#     plt.xlabel('x * g(x)')
+#     plt.ylabel('y')
+#     plt.show()
