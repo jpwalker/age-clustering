4223ea31a2fdf0b464ea83f2dae1cc7845effcc7
jpwalker <jpwalker4039@gmail.com>
2014-08-16 14:12:12 -0400

    8/16/14

--- plots/Mass_age.py
+++ plots/Mass_age.py
@@ -31,7 +31,7 @@ def get_bin_mean(data, b_start, b_end):
     else:
         return (None, idx)
 
-def plot_age(form, fof_np, age_labels, symbls):
+def plot_age(p, form, fof_np, age_labels, symbls):
     lims = 10. ** np.linspace(9, 15, (15.25-9) / .25) #These are the limits for the mass bins
     for (i, form_i) in enumerate(form):
         (avg, med) = binning(fof_np[i], form_i, lims)
@@ -65,67 +65,79 @@ def plot_age(form, fof_np, age_labels, symbls):
             yl.append('')
     y2.set_yticklabels(yl)
     y2.set_ylabel('z + 1')
-    plt.show()
     
-def plot_redshift(redshifts, fof_np, age_labels, symbls):
+def plot_redshift(p, redshifts, fof_np, age_labels, symbls):
     lims = 10. ** np.linspace(9, 15, (15.25-9) / .25)
     for (i, red_i) in enumerate(redshifts): 
         (avg, med) = binning(fof_np[i], red_i, lims)
-        plt.semilogx(avg[0], avg[1], marker = symbls[i], subsx = [2, 3, 4, 5, 6, 7, 8, 9], \
+        p.semilogx(avg[0], avg[1], marker = symbls[i], subsx = [2, 3, 4, 5, 6, 7, 8, 9], \
                      label = 'Average {0}'.format(age_labels[i]))
     #plt.semilogx(med[0], med[1], 'b', linestyle = 'dashed', label = 'Median')
     MS = 2.89 * (avg[0] / 10. ** 10.) ** -0.0563
-    plt.semilogx(avg[0], MS, 'r', label = 'MS Curve')
-    plt.legend()
-    plt.xlabel('M [M_sun / h]')
-    plt.ylabel('z + 1')
+    p.semilogx(avg[0], MS, 'r', label = 'MS Curve')
+    p.legend(fontsize = "small")
+    p.set_xlabel('M [M_sun / h]')
+    p.set_ylabel('z + 1')
     
     ##Create Second x axis
-    xlims = plt.xlim()
-    x2 = plt.twiny()
+    xlims = p.get_xlim()
+    x2 = p.twiny()
     x2.set_xscale('log')
     x2.set_xlim((xlims[0] / .73, xlims[1] / .73))
     x2.set_xlabel('M [M_sun]')
     
     ##Create second y axis
-    ylims = plt.ylim()
-    yt = plt.yticks()[0]
-    y2 = plt.twinx()
+    ylims = p.get_ylim()
+    yt = p.get_yticks()
+    y2 = p.twinx()
     y2.set_ylim(ylims)
     y2.set_yticks(yt)
     yl = []
     for j in yt:
         yl.append('{:5.2f}'.format(13.5795 - mth.asinh((j / 1.4424957031) ** (-3. / 2.)) / 0.0969815))
     y2.set_yticklabels(yl)
-    y2.set_ylabel('Lookbacktime')
-    plt.show()    
+    y2.set_ylabel('Lookbacktime')  
 
+def s_plot(p, fnames, age_keys, age_labels, symbols):
+    for i in range(len(fnames)):
+        #Read in all halos
+        halos = read_halo_table_ascii('{0}{1}'.format(direc, fnames[i]), \
+                                  fmt = 'x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,21,22,23,24,27,25,28,26,29')
+        form = []
+        redshifts = []
+        fof_np = []
+        for j in age_keys:
+            tempmass = np.array(get_col_halo_table(halos, 'fof_np'))
+            tempage = np.array(get_col_halo_table(halos, j))
+            if j == 'merg':
+                idx = np.where(tempage <= 13.5)
+                tempmass = tempmass[idx]
+                tempage = tempage[idx]
+            fof_np.append(tempmass * massconv)
+            form.append(tempage)
+            redshifts.append(1.44224957031 * np.sinh(0.0969815 * (13.5795 - form[-1])) ** (-2. / 3.))
+        #plot_age(p, form, fof_np, age_labels, symbls)
+        plot_redshift(p, redshifts, fof_np, age_labels, symbols)
+            
 if __name__ == '__main__':
     home = '{0}/'.format(os.environ['HOME'])
-    direc = '{0}Desktop/age-clustering-data/'.format(home)
-    age_file = ('attempt1millenniumIIsnap67_1057_fof.txt', 'attempt1millenniumIIsnap67_1057_sub.txt')
-    infile = age_file[1]
-    age_key = ['assem_gao', 'assem_jp']
-    age_labels = ['Sub-Root-Assem. Age', 'Sub-Max_tree-Assem. Age']
-    symbls = ['s', '^']
+    direc = '{0}Desktop/age-clustering-data/snap67/'.format(home)
+    age_file = (('attempt1millenniumIIsnap67_1057_fof.txt', 
+                 'attempt1millenniumIIsnap67_1057_sub.txt'), 
+                ('attempt1millenniumIIsnap67_1057_fof.txt', 
+                 'attempt1millenniumIIsnap67_1057_sub.txt'))
+    age_key = ((('form_gao', 'form_jp'), ('form_gao', 'form_jp')), 
+               (('assem_gao', 'assem_jp'), ('assem_gao', 'assem_jp')))
+    age_labels = ((('FOF-Root-Form. Age', 'FOF-Max_tree-Form. Age'), 
+                   ('Sub-Root-Form. Age', 'Sub-Max_tree-Form. Age')), 
+                  (('FOF-Root-Assem. Age', 'FOF-Max_tree-Assem. Age'), 
+                   ('Sub-Root-Assem. Age', 'Sub-Max_tree-Assem. Age')))
+    symbls = ((('s', '^'), ('s', '^')), (('s', '^'), ('s', '^')))
     h = 0.73
     massconv = 6.885e6 #Mass conversion reports mass in M_sun/h
-    #Read in all halos
-    halos = read_halo_table_ascii('{0}{1}'.format(direc, infile), \
-                                  fmt = 'x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,21,22,23,24,27,25,28,26,29')
-    form = []
-    redshifts = []
-    fof_np = []
-    for i in age_key:
-        tempmass = np.array(get_col_halo_table(halos, 'fof_np'))
-        tempage = np.array(get_col_halo_table(halos, i))
-        if i == 'merg':
-            idx = np.where(tempage <= 13.5)
-            tempmass = tempmass[idx]
-            tempage = tempage[idx]
-        fof_np.append(tempmass * massconv)
-        form.append(tempage) 
-        redshifts.append(1.44224957031 * np.sinh(0.0969815 * (13.5795 - form[-1])) ** (-2. / 3.))
-    plot_age(form, fof_np, age_labels, symbls)
-    plot_redshift(redshifts, fof_np, age_labels, symbls)
-    
+    (fig, ax) = plt.subplots(2, 2, True, True)
+    for (i_idx, i) in enumerate(ax):
+        for (j_idx, j) in enumerate(i):
+            s_plot(j, (age_file[i_idx][j_idx],), age_key[i_idx][j_idx], age_labels[i_idx][j_idx], symbls[i_idx][j_idx])
+    plt.show()
+            
