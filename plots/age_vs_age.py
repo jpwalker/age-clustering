b4680adc3bede1ede0cd05ac6c46008942a026d0
jpwalker <jpwalker@JEANDESK>
2014-11-03 10:31:51 -0500

    1-3-14

--- plots/age_vs_age.py
+++ plots/age_vs_age.py
@@ -30,14 +30,16 @@ def plot_percentiles(data, numbins, xlim, ylim, vert = True, color = 'k', linest
 if __name__ == '__main__':
     home = '{0}/'.format(os.environ['HOME'])
     direc = '{0}Desktop/age-clustering-data/'.format(home)
-    age_file = ('attempt1millenniumIIsnap67_1057_fof.txt', 'attempt1millenniumIIsnap67_1057_sub.txt')
+    age_file = ('attempt1millenniumIIsnap67_1057_fof.txt', \
+                'attempt1millenniumIIsnap67_1057_sub.txt')
     age_file = age_file[1]
     agekeys = ['assem_jp', 'assem_gao']
     lbls = ['Sub-Max_tree-Assem. Age', 'Sub-Root-Assem. Age']
     num_xbins = 30 #Number of bins on x axis. This is used for contour plot.
     num_ybins = 30 #Number of bins on y axis. This is used for contour plot.
+    fmt = 'x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,21,22,23,24,27,25,28,26,29'
     halos = read_halo_table_ascii('{0}{1}'.format(direc, age_file), \
-                                  fmt = 'x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,21,22,23,24,27,25,28,26,29')
+                                  fmt = fmt)
     print 'File read...Ploting...'
     age1 = np.array(get_col_halo_table(halos, agekeys[0]))
     age2 = np.array(get_col_halo_table(halos, agekeys[1]))
@@ -52,7 +54,8 @@ if __name__ == '__main__':
             y[x_i][y_i] = yc
     idx = np.where(age1 > age2)
     plt.plot(age1[idx], age2[idx], '*', markersize = 0.9, color = 'k')
-    rng = [min(np.reshape(np.log10(z[0]), num_xbins * num_ybins)), max(np.reshape(np.log10(z[0]), num_xbins * num_ybins))]
+    rng = [min(np.reshape(np.log10(z[0]), num_xbins * num_ybins)), 
+           max(np.reshape(np.log10(z[0]), num_xbins * num_ybins))]
     if rng[0] == float('-inf'):
         rng[0] = 0.0001
     lvls = np.linspace(0.35 * (rng[1] - rng[0]) + rng[0], rng[1], 15)
