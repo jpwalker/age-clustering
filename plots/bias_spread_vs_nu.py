20b8abe781292bff205a72770d5edc307142f31c
jpwalker <jpwalker@JEANDESK>
2015-03-11 11:06:23 -0400

    3-11-15

--- plots/bias_spread_vs_nu.py
+++ plots/bias_spread_vs_nu.py
@@ -6,9 +6,11 @@ Created on Mar 4, 2015
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from compute_nu_eff import calc_seljak_warren
 from compute_nu_eff import reverse_calc_nu_eff
 from math import exp
+
 
 if __name__ == '__main__':
     cosmo = {'omega_M_0': 0.25, 'omega_lambda_0': 0.75, 'omega_b_0': 0.045, \
@@ -16,13 +18,17 @@ if __name__ == '__main__':
     seljak = calc_seljak_warren(1000, cosmo)
     trans = 1000.
     best_fit = (7.09088257E1, 3.21411038E-2, -7.03202061E1)
-    alpha = (0, 0.5)
-    color = ('blue', 'red')
+    alpha = (0, -0.5, -0.25, 0.25, 0.5)
+    color = ('blue', 'red', 'green', 'black')
     nu0 = [best_fit[0] * exp(best_fit[1] * a) + best_fit[2] for a in alpha]
     nu = np.linspace(0.5, 3.5, 1000)
     nu_eff = [(nu - n0) / (1. + np.exp(-trans * (nu - n0))) + n0 for n0 in nu0]
     bias = [reverse_calc_nu_eff(i, seljak) for i in nu_eff]
     for (i, y) in enumerate(bias):
-        plt.plot(nu,y, color[i])
-    plt.plot(seljak[0], seljak[1], 'k--')
+        if i !=0:
+            plt.plot(nu, y - bias[0], color[i - 1])
+    #plt.plot(seljak[0], seljak[1], 'k--')
+    rc('text', usetex=True)
+    plt.xlabel('$\\nu$')
+    plt.ylabel('$\\Delta b$')
     plt.show()
