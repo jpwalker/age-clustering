7e3404a883b28e44c66cd49cf7a9f47a57ecd582
jpwalker <jpwalker@JEANDESK>
2015-12-11 06:49:03 -0500

    12-11-15

--- Bootstrap/simple_bootstrap.py
+++ Bootstrap/simple_bootstrap.py
@@ -6,10 +6,10 @@ Created on Nov 12, 2015
 
 from MillenniumII import read_halo_table_ascii, select_halo_table,\
     create_halo_table, halo_table_extend
-from os import environ, times, getpid
-from os.path import join
+from os import environ, times, getpid, makedirs
+from os.path import join, exists
 from random import randint, seed, sample, choice
-from Correlation_Func import calc_bias_cross
+from Correlation_Func import calc_bias_cross, read_corr_file
 from threading import Thread
 
 def create_params(num_halos):
@@ -37,15 +37,40 @@ def init_direc():
     i = 0
     j = 1
     snap_suffix = '-1'
+    xi_m_m_fn = 'xi_m_m.txt'
+    xi_all_fn = 'xi_attempt1millenniumII_sub.txt'
+    d_ahalo = 'halo_table_attempt1millenniumII_sub.txt'
     d_snap = join('snap{0}{1}'.format(snap, snap_suffix))
     d_prefix = 'attempt1'
     halo_type = 'sub'
     age_def = 'form_jp'
     d_age = join('{0}_{1}_{2}'.format(d_prefix, halo_type, age_def))
     d_halo = join('halo_tables', 'halo_table_{0}_{1}.dat'.format(i, j))
-    return join(hm, 'Desktop', 'age-clustering-data', d_snap, d_age, d_halo)
+    # Input Files
+    temp = join(hm, 'Desktop', 'age-clustering-data', d_snap, d_age,)
+    d_halo_fl = join(temp, d_halo)
+    d_ahalo_fl = join(temp, d_ahalo)
+    xi_m_m_fl = join(temp, xi_m_m_fn)
+    xi_all_halo_fl = join(temp, xi_all_fn)
+    # Output Files
+    d_BS = join(temp, 'BS_{0}_{1}'.format(i, j))
+    # These are meant to be assigned a BS idx
+    temp = join(d_BS,'halo_tables')
+    if not exists(temp):
+        makedirs(temp)
+    BS_ht_fl = join(temp, 'BS_{0}')
+    temp = join(d_BS, 'cross')
+    if not exists(temp):
+        makedirs(temp)
+    BS_xi_fl = join(temp,'xi_{0}')
+    temp = join(d_BS, 'bias')
+    if not exists(temp):
+        makedirs(temp)
+    BS_b_fl = join(temp, 'bias_{0}')
+    inp = {'ht':d_halo_fl,'a_ht':d_ahalo_fl,'xm':xi_m_m_fl,'xa':xi_all_halo_fl}
+    out = {'ht':BS_ht_fl,'xi':BS_xi_fl,'b':BS_b_fl}
+    return (inp, out)
     
-
 def set_idx1(params):
     idx = []
     temp = [randint(1, params['ns']) for _ in range(params['nh'])]
@@ -70,7 +95,6 @@ def set_idx2(params):
         nh = len(av_idx)
     return tuple(idx)
 
-    
 def create_samples(halos, idxs):
     samples = []
     for idx in idxs:
@@ -87,20 +111,26 @@ def create_bootstraps(params, samples):
             halo_table_extend(BS_samples[-1], choice(samples))
     return BS_samples
 
-#a_halos represents all halos used in the cross-correlation.
-def thread_func(samples, a_halos, xi_m_m, xi_all):
+def thread_func(samples, smp_idx_start, a_halos, xi_m_m, xi_all):
+    #a_halos represents all halos used in the cross-correlation.
+    #xi_all, xi_m_m holds the autocorrelation function of all halos and matter.
     for smp in samples:
         
-        _ = calc_bias_cross(smp, a_halos, xi_m_m, halo_file1 =  cross_filename = '', auto_filename = '', 
+        _ = calc_bias_cross(smp, a_halos, xi_m_m, halo_file1 = '', cross_filename = '', auto_filename = '', 
                     bias_filename = '', xi_auto_halos = None, MS2 = False)
 
-def setup_threads(params, samples, num=8):
+def setup_threads(params, fn_i, fn_o, samples, num=8):
     ns = params['ns']
     thrds = []
     sample_start = 0
     sample_end = ns // num + 1
+    fmt = 'x,x,x,x,x,x,x,7,8,9,x,x,x,x,x,x,x,17,x,x,x'
+    all_ht = read_halo_table_ascii(fn_i['xa'], fmt, skip = 1)
+    xi_m_m = read_corr_file(fn_i['xm'])
+    xi_all = read_corr_file(fn_i['xa'])
     for _ in xrange(num):
-        arg = (samples[sample_start:sample_end], )
+        arg = (samples[sample_start:sample_end], sample_start, all_ht, 
+               xi_m_m, xi_all)
         thrds.append(Thread(target=thread_func, args=arg))
         sample_start = sample_end + 1
         sample_end += ns // num + 1
@@ -109,14 +139,14 @@ def setup_threads(params, samples, num=8):
         
 if __name__ == '__main__':
     fmt = 'x,x,x,x,x,x,x,7,8,9,x,x,x,x,x,x,x,17,x,x,x'
-    fn = init_direc()
-    main_sample = read_halo_table_ascii(fn, fmt, skip = 1)
+    (fn_i, fn_o) = init_direc()
+    main_sample = read_halo_table_ascii(fn_i['ht'], fmt, skip = 1)
     params = create_params(main_sample['length'])
     idxs = set_idx2(params)
     draw_samples = create_samples(main_sample, idxs)
     BS_samples = create_bootstraps(params, draw_samples)
-    setup_threads()
-    for i in BS_samples:
+    setup_threads(params, fn_i, fn_o, BS_samples)
+    #for i in BS_samples:
 #     from matplotlib.pyplot import hist, show
 #     hist(idx, bins = params['ns'])
 #     show()
